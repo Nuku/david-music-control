76a3fbdc2a50da1d7ad37ced587199208de80355
David Silver <dms.silver@gmail.com>
2026-04-30 11:59:19 -0700

    Release v2.4.11

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## [2.4.11] - 2026-04-30
+
+### Removed
+- **Auto Walls prototype**: Removed the scene auto-walling tool and all related browser-side detector code.
+
 ## [2.4.10] - 2026-04-30
 
 ### Fixed
--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import './module/menu.js';
 import './module/token.js';
 import './module/party-cluster.js';
 import './module/scene-music.js';
-import './module/auto-walls.js';
 import { migrate } from './module/migrate.js';
 import('./module/encounter.js');
 
